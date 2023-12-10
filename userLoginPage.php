704ae863bf33fbc90a676a59490268a5f3d34df5
Jowayne Anderson <jowayneaanderson@gmail.com>
2023-12-10 17:23:40 -0500

    Updates

--- userLoginPage.php
+++ userLoginPage.php
@@ -1,34 +1,13 @@
-<?php
-session_start();
-
+<?php 
+    session_start();
+    include "generateCsfr.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
-    <!-- Add any additional styles or scripts here -->
-</head>
-<body>
-
-<form id="loginForm" action="verifyUser.php" method="POST">
+<form id = "loginForm" action="verifyUser.php" method="POST">
     <h1>LOGIN</h1>
 
-    <!-- CSRF Token -->
-    <input type="hidden" name="csrfToken" value="<?php echo $key; ?>">
-
-    <!-- Email and Password Fields -->
-    <label for="email">Email address:</label>
-    <input type="text" id="email" name="email" placeholder="Email address" required><br>
-
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" placeholder="Password" required><br>
-
-    <!-- Login Button -->
-    <button type="submit"><img src="lock.jpg" alt="Black lock icon"> Login</button>
+    <input type="hidden" id= "csrfToken" value= "<?php echo $key; ?>" >
+    <input type="text" id = "email" name="email" placeholder="Email address"><br>
+    <input type="password" id = "password" name="password" placeholder="Password"><br>
+    <button type = "button" id = "loginButton"><img src="lockIcon.png" alt="Black lock icon"> Login</button>
 </form>
-
-</body>
-</html>
