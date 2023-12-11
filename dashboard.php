d59d6a3f0422b29f90c1ff2594da8b69727360fc
adamleemorris <57811531+adamleemorris@users.noreply.github.com>
2023-12-10 22:38:11 -0500

    updates

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<?php  ?>
+
+<div id="loader">
+    <h1> Dashboard</h1>
+    <button id= "addContactButton" type="button">add contact</button>
+</div>
+
+<div id= "filterCon">
+    <img src="filter.png" alt=" filter icon">
+    <p>Filter By:</p>
+    <button class="filterlinks">All</button>
+        <button class="filterlinks">Sales Lead</button>
+        <button class="filterlinks">Support</button>
+        <button class="filterlinks">Assigned to me</button>
+    </div>
+
